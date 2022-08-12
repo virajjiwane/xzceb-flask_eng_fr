258d65ae5a4b051837aacdc16433f0ae4f788831
virajjiwane <vjiwane27@gmail.com>
2022-08-12 16:56:09 +0000

    added translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,11 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
