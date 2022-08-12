2a016984b55a60d9b53a4a51e392f212fd22acb7
virajjiwane <vjiwane27@gmail.com>
2022-08-12 19:08:01 +0000

    last

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,20 +1,20 @@
 import unittest
 
-from final_project.machinetranslation.translator import english_to_french, french_to_english
+from machinetranslation import translator
 
 
 class TranslatorModuleTest(unittest.TestCase):
     def test_english_to_french_null_input(self):
-        self.assertRaises(Exception, english_to_french, None)
+        self.assertRaises(Exception, translator.english_to_french, None)
 
     def test_french_to_english_null_input(self):
-        self.assertRaises(Exception, french_to_english, None)
+        self.assertRaises(Exception, translator.french_to_english, None)
 
     def test_english_to_french(self):
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertEqual(translator.english_to_french('Hello'), 'Bonjour')
 
     def test_french_to_english(self):
-        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertEqual(translator.french_to_english('Bonjour'), 'Hello')
 
 
 if __name__=='__main__':
