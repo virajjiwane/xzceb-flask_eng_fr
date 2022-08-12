4273ba2322802ae3c5a7711729c4daf6b4700466
virajjiwane <vjiwane27@gmail.com>
2022-08-12 17:44:25 +0000

    pylint

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,10 +1,19 @@
 import unittest
 
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
 class TranslatorModuleTest(unittest.TestCase):
-    def test_englishToFrench(self):
-        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+    def test_english_to_french_null_input(self):
+        self.assertRaises(Exception, english_to_french, None)
+
+    def test_french_to_english_null_input(self):
+        self.assertRaises(Exception, french_to_english, None)
+
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
 
 if __name__=='__main__':
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
-import json
+"""Module providing functions to translate text between english and french"""
+import os
+
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,17 +18,22 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-
-def englishToFrench(englishText):
+def english_to_french(english_text):
+    """Function translating english text to french."""
+    if not english_text:
+        raise Exception('english_text should not be null')
     translation = language_translator.translate(
-        text=englishText, 
+        text=english_text,
         model_id='en-fr').get_result()
-    frenchText = translation.get('translations')[0].get('translation')
-    return frenchText
+    french_text = translation.get('translations')[0].get('translation')
+    return french_text
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
+    """Function translating french text to english."""
+    if not french_text:
+        raise Exception('french_text should not be null')
     translation = language_translator.translate(
-        text=frenchText, 
+        text=french_text,
         model_id='fr-en').get_result()
-    englishText = translation.get('translations')[0].get('translation')
-    return englishText
+    english_text = translation.get('translations')[0].get('translation')
+    return english_text
